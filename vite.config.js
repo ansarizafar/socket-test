cc4057809bd50f4a29ef9d71e84d1825c5b54a2c
Joseph Werle <joseph.werle@gmail.com>
2023-12-05 15:08:04 +0100

    refactor(): fix build setup

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,26 @@
-import imba from 'imba/plugin';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
 import externalize from 'vite-plugin-externalize-dependencies'
+import imba from 'imba/plugin'
 
 export default defineConfig({
-	plugins: [
-	imba(),
-	externalize({
-	externals: [/^socket:.*/], 
-  }),
-	],
-	
-	resolve: {
-		dedupe:['imba']
-	  },
-	  
-	build: {
-		outDir: 'imba', 
-		rollupOptions: {
-		 /*
-			external(id) {
-			  return id.startsWith('socket:')
-			}
-			*/
-		external: [/^socket:.*/],
-		  }
-	  },
-});
+  plugins: [
+    imba(),
+    externalize({
+      externals: [/^socket:.*/]
+    })
+  ],
+
+  resolve: {
+    dedupe: ['imba']
+  },
+
+  build: {
+    outDir: 'build/imba',
+    rollupOptions: {
+      external: [/^socket:.*/],
+      input: {
+        app: 'src/index.html'
+      }
+    }
+  }
+})
